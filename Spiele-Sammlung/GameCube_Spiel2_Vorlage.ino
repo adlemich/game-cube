2825d55e0c7a68fbd0d5be8cc7094078a3aeaac1
Michael <50623619+adlemich@users.noreply.github.com>
2021-08-05 12:38:20 +0200

    Added Template and the Pong Game

---
+++ Spiele-Sammlung/GameCube_Spiel2_Vorlage.ino
@@ -0,0 +1,55 @@
+/*
+ * Leere Vorlage mit den leeren Funktions-Definitionen.
+ */
+
+//------------------------------------------------------------------------------------------------------------
+// GLOABLE VARIABLEN - Hier legen wir Variablen, also Daten-Elemente an die wir im Spiel nutzen
+//------------------------------------------------------------------------------------------------------------
+
+// ------------------------------------------------------------------------------------------------------------
+// spiel_2_start - Legt fest was passiert wenn das Spiel gestartet wird
+//  
+// ------------------------------------------------------------------------------------------------------------
+FUNKTION spiel_2_start() { 
+  
+}
+
+// ------------------------------------------------------------------------------------------------------------
+// spiel_2_taster_gruen_oben - Legt fest was passiert wenn der grüne, obere Taster gedrückt wird
+// 
+// ------------------------------------------------------------------------------------------------------------
+FUNKTION spiel_2_taster_gruen_oben() { 
+
+}
+
+// ------------------------------------------------------------------------------------------------------------
+// spiel_2_taster_gruen_unten - Legt fest was passiert wenn der grüne, untere Taster gedrückt wird
+// 
+// ------------------------------------------------------------------------------------------------------------
+FUNKTION spiel_2_taster_gruen_unten() { 
+   
+}
+
+// ------------------------------------------------------------------------------------------------------------
+// spiel_2_taster_rot_oben - Legt fest was passiert wenn der rote, obere Taster gedrückt wird
+// 
+// ------------------------------------------------------------------------------------------------------------
+FUNKTION spiel_2_taster_rot_oben() { 
+
+}
+
+// ------------------------------------------------------------------------------------------------------------
+// spiel_2_taster_rot_unten - Legt fest was passiert wenn der rote, untere Taster gedrückt wird
+// 
+// ------------------------------------------------------------------------------------------------------------
+FUNKTION spiel_2_taster_rot_unten() {
+  
+}
+
+// ------------------------------------------------------------------------------------------------------------
+// spiel_2_loop - Diese Funktion wird immer dann aufgerufen wenn kein Taster gedrückt wurde, hier läuft die Hauptroutine
+// ------------------------------------------------------------------------------------------------------------
+FUNKTION spiel_2_loop() { 
+  // hier passiert nix. Wir pausieren nur für 1 Millisekunde um den Prozessor zu entlasten
+  WARTE(1);
+}
